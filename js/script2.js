85d226bc759e9e1a449e8198f8b54888e0a6bd02
Laura Bigoni <laurabigoni.92@gmail.com>
2022-01-20 16:02:23 +0100

    setto le proprietà a 0

--- js/script2.js
+++ js/script2.js
@@ -17,15 +17,16 @@ const display = document.getElementById('display');
 
 // # CREO UN ARRAY DI OGGETTI DI SQUADRE DI CALCIO
 const footballTeams = [
-    { nome: 'Salernitana', punti_fatti: 11, falli_subiti: 12 },
-    { nome: 'Inter', punti_fatti: 80, falli_subiti: 14 },
-    { nome: 'Fiorentina', punti_fatti: 31, falli_subiti: 7 },
-    { nome: 'Milan', punti_fatti: 63, falli_subiti: 9 },
-    { nome: 'Napoli', punti_fatti: 59, falli_subiti: 11 },
-    { nome: 'Empoli', punti_fatti: 19, falli_subiti: 6 },
-    { nome: 'Udinese', punti_fatti: 15, falli_subiti: 10 },
-    { nome: 'Lazio', punti_fatti: 41, falli_subiti: 18 },
-    { nome: 'Bologna', punti_fatti: 21, falli_subiti: 5 },
-    { nome: 'Roma', punti_fatti: 58, falli_subiti: 13 },
+    { nome: 'Salernitana', punti_fatti: 0, falli_subiti: 0 },
+    { nome: 'Inter', punti_fatti: 0, falli_subiti: 0 },
+    { nome: 'Fiorentina', punti_fatti: 0, falli_subiti: 0 },
+    { nome: 'Milan', punti_fatti: 0, falli_subiti: 0 },
+    { nome: 'Napoli', punti_fatti: 0, falli_subiti: 0 },
+    { nome: 'Empoli', punti_fatti: 0, falli_subiti: 0 },
+    { nome: 'Udinese', punti_fatti: 0, falli_subiti: 0 },
+    { nome: 'Lazio', punti_fatti: 0, falli_subiti: 0 },
+    { nome: 'Bologna', punti_fatti: 0, falli_subiti: 0 },
+    { nome: 'Roma', punti_fatti: 0, falli_subiti: 0 },
 ];
 console.table(footballTeams);
+
