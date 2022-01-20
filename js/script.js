a38c679ee5c1184abfb44454a41c02ac6269e84d
Laura Bigoni <laurabigoni.92@gmail.com>
2022-01-20 15:51:05 +0100

    consegna snack 1

--- js/script.js
+++ js/script.js
@@ -5,27 +5,48 @@ Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 
 Stampare in console la bici con peso minore utilizzando destructuring e template literal
+
+BONUS
+Stampare in pagina oltre che in console!
 */
+// # RECUPERO GLI ELEMENTI IN PAGINA
+const display = document.getElementById('display');
+
 
 // # CREO UN ARRAY DI OGGETTI DI BICICLETTE
 const bikes = [
-    { nome: 'Tommasini', peso: 0.43 },
+    { nome: 'Tommasini', peso: 7.43 },
     { nome: 'Saccarelli', peso: 1.25 },
-    { nome: 'Ridley', peso: 1.10 },
+    { nome: 'Ridley', peso: 3.10 },
     { nome: 'Pedemonte', peso: 2.00 },
     { nome: 'Stelbel', peso: 0.96 },
-    { nome: 'Vetta', peso: 0.98 },
+    { nome: 'Vetta', peso: 1.98 },
     { nome: 'Trek', peso: 1.50 },
-    { nome: 'MCipollini', peso: 2.10 },
+    { nome: 'MCipollini', peso: 4.10 },
     { nome: 'Formigli', peso: 1.40 },
-    { nome: 'Bianchi', peso: 1.85 },
+    { nome: 'Bianchi', peso: 5.85 },
 ];
-
 console.table(bikes);
 
+
 // # STAMPO IN CONSOLE LA BICI CON PESO MIN
-const minWeight = bikes.reduce((val1, val2) => val1.peso < val2.peso ? val1 : val2);
-console.log(minWeight)
+let minWeight = 0;
+for (let i = 0; i < bikes.length; i++) {
+const currentWeight = bikes[i].peso;
+if (i == 0) {
+    minWeight = currentWeight;
+}
+if (currentWeight < minWeight) {
+    minWeight = currentWeight;
+}
+};
+console.log(minWeight);
+
+const bikeName = bikes.find(bikeName => bikeName.peso === minWeight);
+console.log(bikeName);
+const {nome} = bikeName;
+console.log(nome);
 
 
-//
+// # STAMPO IN PAGINA
+display.innerHTML = `La bici col peso minore è:<strong> ${nome.toUpperCase()} </strong><br> con un peso di:<strong> ${minWeight} </strong>kg`;
--- js/script2.js
+++ js/script2.js
@@ -7,4 +7,7 @@ Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0
 Generare numeri random al posto degli 0 nelle proprietà:
 Punti fatti e falli subiti.
 Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+BONUS
+Stampare in pagina oltre che in console!
 */
