a84702d2c742b0d9c03eeb194d7125264388634e
Laura Bigoni <laurabigoni.92@gmail.com>
2022-01-20 16:01:08 +0100

    snack 2- creo un array di oggetti

--- js/script2.js
+++ js/script2.js
@@ -11,3 +11,21 @@ Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengo
 BONUS
 Stampare in pagina oltre che in console!
 */
+// # RECUPERO GLI ELEMENTI IN PAGINA
+const display = document.getElementById('display');
+
+
+// # CREO UN ARRAY DI OGGETTI DI SQUADRE DI CALCIO
+const footballTeams = [
+    { nome: 'Salernitana', punti_fatti: 11, falli_subiti: 12 },
+    { nome: 'Inter', punti_fatti: 80, falli_subiti: 14 },
+    { nome: 'Fiorentina', punti_fatti: 31, falli_subiti: 7 },
+    { nome: 'Milan', punti_fatti: 63, falli_subiti: 9 },
+    { nome: 'Napoli', punti_fatti: 59, falli_subiti: 11 },
+    { nome: 'Empoli', punti_fatti: 19, falli_subiti: 6 },
+    { nome: 'Udinese', punti_fatti: 15, falli_subiti: 10 },
+    { nome: 'Lazio', punti_fatti: 41, falli_subiti: 18 },
+    { nome: 'Bologna', punti_fatti: 21, falli_subiti: 5 },
+    { nome: 'Roma', punti_fatti: 58, falli_subiti: 13 },
+];
+console.table(footballTeams);
