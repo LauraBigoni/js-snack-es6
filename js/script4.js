ddf870797127fa5251b23beefae3f2d3db3ce4da
Laura Bigoni <laurabigoni.92@gmail.com>
2022-01-21 16:28:27 +0100

    creo l'array genero num random e li assegno ai val

--- js/script4.js
+++ js/script4.js
@@ -12,3 +12,38 @@ BONUS  nei nomi sulle targhe l'iniziale di ogni parola deve essere maiuscola
 ES.: (marco de iulio  => Marco De Iulio)
 */
 
+// # RECUPERO GLI ELEMENTI IN PAGINA
+const display = document.getElementById('display');
+
+
+const studenti = [
+    {id: 0, nome: 'luca bianchi', voto: 0},
+    {id: 0, nome: 'marco de iulio', voto: 0},
+    {id: 0, nome: 'stefano cesaroni', voto: 0},
+    {id: 0, nome: 'anna mazzoni', voto: 0},
+    {id: 0, nome: 'ilaria marini', voto: 0},
+    {id: 0, nome: 'stefano truncellito', voto: 0},
+    {id: 0, nome: 'davide sesto', voto: 0},
+    {id: 0, nome: 'stefania rosa', voto: 0},
+    {id: 0, nome: 'valentino rossi', voto: 0},
+    {id: 0, nome: 'maria lo iudice', voto: 0},
+];
+
+console.table(studenti);
+
+
+// # FUNZIONE NUM RANDOM
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+
+// # GENERO NUM RANDOM DA INSERIRE NELLE PROPRIETA'
+let id;
+let voto;
+
+for (let i = 0; i < studenti.length; i++) {
+    let studente = studenti[i];
+    studente.id = getRandomNumber(100, 500);
+    studente.voto = getRandomNumber(20, 100);
+}
+
+console.table(studenti);
